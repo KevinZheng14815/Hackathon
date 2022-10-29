0ff88b30fb80df323ad321e9182e700185244846
Kevin Zheng <91815800+KevinZheng14815@users.noreply.github.com>
2022-10-29 17:07:19 -0500

    Fix some part 1 code stuff

--- api.py
+++ api.py
@@ -35,4 +35,4 @@ from werzeug.wrappers import Request, Response
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app)   
