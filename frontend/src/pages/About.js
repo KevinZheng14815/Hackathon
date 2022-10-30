2266c88ef2d63a55ac27d870f18d5a9ce953ac57
Max-Laptop <92011471+fygure@users.noreply.github.com>
2022-10-30 10:28:39 -0500

    frontend complete-o

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import max from '../static/img/max.png';
+import farrah from '../static/img/farrah.png';
+import khan from '../static/img/khan.png';
+import kevin from '../static/img/kevin.png';
+
+const About = () => {
+    return (
+<div class="mb-16">
+            {/* <!-- Code block starts --> */}
+            <dh-component>
+                <div class="container flex justify-center mx-auto pt-16">
+                    <div>
+                        <h1 class="xl:text-4xl text-3xl text-center text-gray-800 font-extrabold">About Us</h1>
+                    </div>
+                </div>
+                <div class="w-full px-10 pt-10">
+                    <div class="container mx-auto">
+                        <div role="list" aria-label="Behind the scenes People " class="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around">
+                            <div role="listitem" class="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                                <div class="rounded overflow-hidden shadow-md bg-white">
+                                    <div class="absolute -mt-20 w-full flex justify-center">
+                                        <div class="h-32 w-32">
+                                            <img src={khan} alt="Display Picture of Haroon Khan" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                        </div>
+                                    </div>
+                                    <div class="px-6 mt-16">
+                                        <h1 class="font-bold text-3xl text-center mb-1">Mohammed Khan</h1>
+                                        <p class="text-gray-800 text-sm text-center">Software Connoisseur</p>
+                                        <div class="w-full flex justify-center pt-5 pb-5">
+                                            <a href="https://github.com/MohaKh1" class="mx-5">
+                                                <div aria-label="Github" role="img">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div role="listitem" class="xl:w-1/3 lg:mx-3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                                <div class="rounded overflow-hidden shadow-md bg-white">
+                                    <div class="absolute -mt-20 w-full flex justify-center">
+                                        <div class="h-32 w-32">
+                                            <img src={max} alt="Display Picture of Max Chalitsios" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                        </div>
+                                    </div>
+                                    <div class="px-6 mt-16">
+                                        <h1 class="font-bold text-3xl text-center mb-1">Maximillian Chalitsios</h1>
+                                        <p class="text-gray-800 text-sm text-center">CEO of Tryhards</p>
+                                        <p class="text-center text-gray-600 text-base pt-3 font-normal"></p>
+                                        <div class="w-full flex justify-center pt-5 pb-5">
+                                            <a href="https://github.com/fygure" class="mx-5">
+                                                <div aria-label="Github" role="img">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div role="listitem" class="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                                <div class="rounded overflow-hidden shadow-md bg-white">
+                                    <div class="absolute -mt-20 w-full flex justify-center">
+                                        <div class="h-32 w-32">
+                                            <img src={farrah} alt="Display Picture of Mohammed Farah" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                        </div>
+                                    </div>
+                                    <div class="px-6 mt-16">
+                                        <h1 class="font-bold text-3xl text-center mb-1">Mohammed Farah</h1>
+                                        <p class="text-gray-800 text-sm text-center">Software Wizard</p>
+                                        <div class="w-full flex justify-center pt-5 pb-5">
+                                            <a href="https://github.com/mafarah1" class="mx-5">
+                                                <div aria-label="Github" role="img">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div role="listitem" class="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                                <div class="rounded overflow-hidden shadow-md bg-white">
+                                    <div class="absolute -mt-20 w-full flex justify-center">
+                                        <div class="h-32 w-32">
+                                            <img src={kevin} alt="Display Picture of Kevin Zheng" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                        </div>
+                                    </div>
+                                    <div class="px-6 mt-16">
+                                        <h1 class="font-bold text-3xl text-center mb-1">Kevin Zheng</h1>
+                                        <p class="text-gray-800 text-sm text-center">Logistical Provider</p>
+                                        <div class="w-full flex justify-center pt-5 pb-5">
+                                            <a href="https://github.com/KevinZheng14815" class="mx-5">
+                                                <div aria-label="Github" role="img">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+            </dh-component>
+            {/* <!-- Code block ends -->
+            <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ ---> */}
+        </div>
+    );
+}
+
+
+export default About;
